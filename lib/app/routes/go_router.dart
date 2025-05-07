93fb7871560d5a2314bf5338e48ea842f8bd6594
Mohsinfida007 <mohsinfida9065@gmail.com>
2025-05-07 21:40:32 +0500

    made apk

--- lib/app/routes/go_router.dart
+++ lib/app/routes/go_router.dart
@@ -28,7 +28,7 @@ import 'package:suprapp/app/get_started/pages/splash_screen.dart';
 
 class MyAppRouter {
   static final router = GoRouter(
-    initialLocation: '/${AppRoute.homePage}',
+    initialLocation: '/${AppRoute.splashScreen}',
     routes: [
       GoRoute(
         name: AppRoute.splashScreen,
