e2b3bbc02b0424a267f44cb5448cd387b9b5654d
Mohsinfida007 <mohsinfida9065@gmail.com>
2025-05-07 19:54:27 +0500

    set main screen

--- lib/app/features/home/widgets/categories_gridview.dart
+++ lib/app/features/home/widgets/categories_gridview.dart
@@ -6,91 +6,41 @@ class CategoriesGridview extends StatelessWidget {
 
   final List<Map<String, String>> items;
 
-  // void _handleNavigation(BuildContext context, String categoryName) {
-  //   switch (categoryName) {
-  //     case 'Rides':
-  //       Navigator.pushNamed(context, AppRoutes.selectAddressScreen);
-  //       break;
-  //     case 'Electronics':
-  //       Navigator.push(context,
-  //           MaterialPageRoute(builder: (context) => QuickElectronicsScreen()));
-  //       break;
-  //     case 'Food':
-  //       Navigator.pushNamed(context, AppRoutes.foodScreen);
-  //       break;
-  //     case 'DineOut':
-  //       Navigator.push(
-  //           context, MaterialPageRoute(builder: (context) => DineOutPage()));
-  //       break;
-  //     case 'Groceries':
-  //       Navigator.push(
-  //           context, MaterialPageRoute(builder: (context) => GroceryScreen()));
-  //       break;
-  //     case 'Shops':
-  //       Navigator.pushNamed(context, AppRoutes.shopsScreen);
-  //       break;
-  //     case 'Send Money':
-  //       Navigator.push(
-  //           context, MaterialPageRoute(builder: (context) => ComingSoonPage()));
-  //       break;
-  //     case 'All Services':
-  //       Navigator.push(
-  //           context,
-  //           MaterialPageRoute(
-  //             builder: (context) => AllServicesPage(),
-  //           ));
-  //       break;
-  //     default:
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(content: Text('No screen defined for $categoryName')),
-  //       );
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double horizontalPadding = 32;
+    final double spacing = 10 * 3;
+    final double tileWidth = (screenWidth - horizontalPadding - spacing) / 4;
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: SizedBox(
-        child: GridView.builder(
-          padding: const EdgeInsets.all(8),
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 25,
-            childAspectRatio: 0.8,
-          ),
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            final item = items[index];
-            // final isThirdItem = index == 2;
-            // final isSixthItem = index == 5;
-            final imageHeight = 63;
-            //  isThirdItem
-            //     ? 67.0
-            //     : isSixthItem
-            //         ? 48.0
-            //         : 55.0;
-
-            final imageWidth = 63;
-            //  isThirdItem
-            //     ? 106.0
-            //     : isSixthItem
-            //         ? 58.0
-            //         : 60.0;
-
-            return ItemGridTile(
-              imagePath: item['pictureUrl']!,
-              title: item['name']!,
-              height: imageHeight.toDouble(),
-              width: imageWidth.toDouble(),
-              onTap: () {},
-              // onTap: () => _handleNavigation(context, item['name']!),
-            );
-          },
+      child: GridView.builder(
+        padding: const EdgeInsets.all(8),
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 25,
+          childAspectRatio: 0.8,
         ),
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          // 3rd (index 2), 7th (index 6), 8th (index 7)
+          final specialIndexes = [2, 6, 7];
+          final scaleFactor = specialIndexes.contains(index) ? 1.7 : 1.0;
+
+          return ItemGridTile(
+            imagePath: item['pictureUrl']!,
+            title: item['name']!,
+            tileWidth: tileWidth,
+            imageScaleFactor: scaleFactor,
+            onTap: () {},
+          );
+        },
       ),
     );
   }
