83986e2045617a9f74b4603a5de413944f7e6b59
Mohsinfida007 <mohsinfida9065@gmail.com>
2025-05-22 16:48:20 +0500

    adjusted product card

--- lib/app/features/groceries/tabs/exclusive_tab.dart
+++ lib/app/features/groceries/tabs/exclusive_tab.dart
@@ -9,165 +9,169 @@ class ExclusiveTab extends StatefulWidget {
 }
 
 class _ExclusiveTabState extends State<ExclusiveTab> {
-  final List<Map<String, String>> allDeals = [
-    {
-      'title': 'Pure Organic Honey Jar',
-      'price': '\$10',
-      'discount': '15% OFF',
-      'old': '\$12',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQavcMVvrXn7KoldH8-1IiGNwLbh0mFtskAgA&s',
-    },
-    {
-      'title': 'Unsweetened Almond Milk Pack',
-      'price': '\$6',
-      'discount': '10% OFF',
-      'old': '\$7',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrXh06hIlgR0eXl4lKBHgaz9kmS--IkgmHoQ&s',
-    },
-    {
-      'title': 'Thick Greek Yogurt Tub',
-      'price': '\$4',
-      'discount': '12% OFF',
-      'old': '\$5',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRib_d0P13XP13Mypnc8tu893qOD9Rl-MdILg&s',
-    },
-    {
-      'title': 'High-Fiber Oats Breakfast Pack',
-      'price': '\$5',
-      'discount': '18% OFF',
-      'old': '\$6',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJtZEE5zxcfq-nAlS_5fJxJrlcjlnxLWOPMQ&s',
-    },
-    {
-      'title': 'Premium Green Tea Bags',
-      'price': '\$8',
-      'discount': '20% OFF',
-      'old': '\$10',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRunZcCzD4RN1X8jK2u5G9VjfFcGExuA5i4dQ&s',
-    },
-    {
-      'title': 'Chocolate Flavored Protein Bar',
-      'price': '\$3',
-      'discount': '10% OFF',
-      'old': '\$3.5',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx2-LEkNXJNKwlafI9IkC-ZUMcs7OXfptRGw&s',
-    },
-    {
-      'title': 'Freshly Picked Strawberries Box',
-      'price': '\$5',
-      'discount': '15% OFF',
-      'old': '\$6',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4qZv4mf12HcEcm7xaZRiHVwJrQXpd4JgBiA&s',
-    },
-    {
-      'title': 'Healthy Mixed Nuts Jar',
-      'price': '\$12',
-      'discount': '25% OFF',
-      'old': '\$16',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfETfsxW8wJujgwP0wqlCM_t3RwZykE3y1vg&s',
-    },
-    {
-      'title': 'Natural Coconut Water Bottle',
-      'price': '\$2',
-      'discount': '5% OFF',
-      'old': '\$2.2',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp8bRiOhsb-UKLtNVe8Kvya31CVmYId2MRCg&s',
-    },
-    {
-      'title': 'Crunchy Granola Trail Mix',
-      'price': '\$7',
-      'discount': '17% OFF',
-      'old': '\$8.5',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4qZv4mf12HcEcm7xaZRiHVwJrQXpd4JgBiA&s',
-    },
-    {
-      'title': 'Dark Cocoa Chocolate Bar',
-      'price': '\$4',
-      'discount': '10% OFF',
-      'old': '\$4.5',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfETfsxW8wJujgwP0wqlCM_t3RwZykE3y1vg&s',
-    },
-    {
-      'title': 'Whole Chia Seeds Pouch',
-      'price': '\$6',
-      'discount': '15% OFF',
-      'old': '\$7',
-      'image': 'https://cdn-icons-png.flaticon.com/512/3064/3064161.png',
-    },
-    {
-      'title': 'Natural Energy Drink Can',
-      'price': '\$3',
-      'discount': '10% OFF',
-      'old': '\$3.3',
-      'image': 'https://cdn-icons-png.flaticon.com/512/706/706164.png',
-    },
-    {
-      'title': 'Gluten-Free Rice Cakes',
-      'price': '\$2',
-      'discount': '8% OFF',
-      'old': '\$2.2',
-      'image': 'https://cdn-icons-png.flaticon.com/512/3303/3303894.png',
-    },
-    {
-      'title': 'Cold-Pressed Avocado Oil',
-      'price': '\$9',
-      'discount': '20% OFF',
-      'old': '\$11',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbU0uYwyRqHXYWAaTC8UJOaQtBD4oizlCE2Q&s',
-    },
-    {
-      'title': 'Probiotic Kombucha Bottle',
-      'price': '\$4',
-      'discount': '10% OFF',
-      'old': '\$4.5',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWvo5gawyT3ciPZz3TXAxpRYutChp7_VHqxg&s',
-    },
-    {
-      'title': 'Red Lentil Pasta Pack',
-      'price': '\$6',
-      'discount': '12% OFF',
-      'old': '\$7',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8d2IYYe7usBxeyzKCyVESImqcCENieL1pLw&s',
-    },
-    {
-      'title': 'Smooth Peanut Butter Jar',
-      'price': '\$5',
-      'discount': '10% OFF',
-      'old': '\$5.5',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjjpf6uemrYZC16p9XjA8xvqQfWRHMIXMKFA&s',
-    },
-    {
-      'title': 'Protein-Rich Quinoa Pack',
-      'price': '\$7',
-      'discount': '15% OFF',
-      'old': '\$8.5',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMIh-OsMjJMEfnxl3IIhIr2bIWUmkEXRikWw&s',
-    },
-    {
-      'title': 'Mixed Fruit Juice Bottle',
-      'price': '\$3',
-      'discount': '5% OFF',
-      'old': '\$3.2',
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfETfsxW8wJujgwP0wqlCM_t3RwZykE3y1vg&s',
-    },
-  ];
+ final List<Map<String, String>> allExclusive = [
+  {
+    'id': 'item_001',
+    'title': 'Pure Organic Honey Jar',
+    'price': '\$10',
+    'discount': '15% OFF',
+    'old': '\$12',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQavcMVvrXn7KoldH8-1IiGNwLbh0mFtskAgA&s',
+  },
+  {
+    'id': 'item_002',
+    'title': 'Unsweetened Almond Milk Pack',
+    'price': '\$6',
+    'discount': '10% OFF',
+    'old': '\$7',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrXh06hIlgR0eXl4lKBHgaz9kmS--IkgmHoQ&s',
+  },
+  {
+    'id': 'item_003',
+    'title': 'Thick Greek Yogurt Tub',
+    'price': '\$4',
+    'discount': '12% OFF',
+    'old': '\$5',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRib_d0P13XP13Mypnc8tu893qOD9Rl-MdILg&s',
+  },
+  {
+    'id': 'item_004',
+    'title': 'High-Fiber Oats Breakfast Pack',
+    'price': '\$5',
+    'discount': '18% OFF',
+    'old': '\$6',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJtZEE5zxcfq-nAlS_5fJxJrlcjlnxLWOPMQ&s',
+  },
+  {
+    'id': 'item_005',
+    'title': 'Premium Green Tea Bags',
+    'price': '\$8',
+    'discount': '20% OFF',
+    'old': '\$10',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRunZcCzD4RN1X8jK2u5G9VjfFcGExuA5i4dQ&s',
+  },
+  {
+    'id': 'item_006',
+    'title': 'Chocolate Flavored Protein Bar',
+    'price': '\$3',
+    'discount': '10% OFF',
+    'old': '\$3.5',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx2-LEkNXJNKwlafI9IkC-ZUMcs7OXfptRGw&s',
+  },
+  {
+    'id': 'item_007',
+    'title': 'Freshly Picked Strawberries Box',
+    'price': '\$5',
+    'discount': '15% OFF',
+    'old': '\$6',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4qZv4mf12HcEcm7xaZRiHVwJrQXpd4JgBiA&s',
+  },
+  {
+    'id': 'item_008',
+    'title': 'Healthy Mixed Nuts Jar',
+    'price': '\$12',
+    'discount': '25% OFF',
+    'old': '\$16',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfETfsxW8wJujgwP0wqlCM_t3RwZykE3y1vg&s',
+  },
+  {
+    'id': 'item_009',
+    'title': 'Natural Coconut Water Bottle',
+    'price': '\$2',
+    'discount': '5% OFF',
+    'old': '\$2.2',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp8bRiOhsb-UKLtNVe8Kvya31CVmYId2MRCg&s',
+  },
+  {
+    'id': 'item_010',
+    'title': 'Crunchy Granola Trail Mix',
+    'price': '\$7',
+    'discount': '17% OFF',
+    'old': '\$8.5',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4qZv4mf12HcEcm7xaZRiHVwJrQXpd4JgBiA&s',
+  },
+  {
+    'id': 'item_011',
+    'title': 'Dark Cocoa Chocolate Bar',
+    'price': '\$4',
+    'discount': '10% OFF',
+    'old': '\$4.5',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfETfsxW8wJujgwP0wqlCM_t3RwZykE3y1vg&s',
+  },
+  {
+    'id': 'item_012',
+    'title': 'Whole Chia Seeds Pouch',
+    'price': '\$6',
+    'discount': '15% OFF',
+    'old': '\$7',
+    'image': 'https://cdn-icons-png.flaticon.com/512/3064/3064161.png',
+  },
+  {
+    'id': 'item_013',
+    'title': 'Natural Energy Drink Can',
+    'price': '\$3',
+    'discount': '10% OFF',
+    'old': '\$3.3',
+    'image': 'https://cdn-icons-png.flaticon.com/512/706/706164.png',
+  },
+  {
+    'id': 'item_014',
+    'title': 'Gluten-Free Rice Cakes',
+    'price': '\$2',
+    'discount': '8% OFF',
+    'old': '\$2.2',
+    'image': 'https://cdn-icons-png.flaticon.com/512/3303/3303894.png',
+  },
+  {
+    'id': 'item_015',
+    'title': 'Cold-Pressed Avocado Oil',
+    'price': '\$9',
+    'discount': '20% OFF',
+    'old': '\$11',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbU0uYwyRqHXYWAaTC8UJOaQtBD4oizlCE2Q&s',
+  },
+  {
+    'id': 'item_016',
+    'title': 'Probiotic Kombucha Bottle',
+    'price': '\$4',
+    'discount': '10% OFF',
+    'old': '\$4.5',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWvo5gawyT3ciPZz3TXAxpRYutChp7_VHqxg&s',
+  },
+  {
+    'id': 'item_017',
+    'title': 'Red Lentil Pasta Pack',
+    'price': '\$6',
+    'discount': '12% OFF',
+    'old': '\$7',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8d2IYYe7usBxeyzKCyVESImqcCENieL1pLw&s',
+  },
+  {
+    'id': 'item_018',
+    'title': 'Smooth Peanut Butter Jar',
+    'price': '\$5',
+    'discount': '10% OFF',
+    'old': '\$5.5',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjjpf6uemrYZC16p9XjA8xvqQfWRHMIXMKFA&s',
+  },
+  {
+    'id': 'item_019',
+    'title': 'Protein-Rich Quinoa Pack',
+    'price': '\$7',
+    'discount': '15% OFF',
+    'old': '\$8.5',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMIh-OsMjJMEfnxl3IIhIr2bIWUmkEXRikWw&s',
+  },
+  {
+    'id': 'item_020',
+    'title': 'Mixed Fruit Juice Bottle',
+    'price': '\$3',
+    'discount': '5% OFF',
+    'old': '\$3.2',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfETfsxW8wJujgwP0wqlCM_t3RwZykE3y1vg&s',
+  },
+];
+
   @override
   Widget build(BuildContext context) {
     const double spacing = 30;
@@ -181,7 +185,7 @@ class _ExclusiveTabState extends State<ExclusiveTab> {
           children: [
             Expanded(
               child: GridView.builder(
-                itemCount: allDeals.length,
+                itemCount: allExclusive.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: crossAxisCount,
                   crossAxisSpacing: spacing,
@@ -189,15 +193,16 @@ class _ExclusiveTabState extends State<ExclusiveTab> {
                   childAspectRatio: 165 / 210,
                 ),
                 itemBuilder: (context, index) {
-                  final item = allDeals[index];
+                  final item = allExclusive[index];
                   return ProductCard(
+                    id: item['id']!,
                     title: item['title']!,
                     price: item['price']!,
                     discount: item['discount']!,
                     oldPrice: item['old'],
                     imageUrl: item['image']!,
                     showOldPrice: true,
-                    onAddTap: () {},
+                    onTap: () {},
                   );
                 },
               ),
--- lib/app/features/groceries/widgets/product_card.dart
+++ lib/app/features/groceries/widgets/product_card.dart
@@ -1,47 +1,53 @@
-import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:suprapp/app/features/groceries/controllers/product_quantity_provider.dart';
 
 class ProductCard extends StatelessWidget {
+  final String id;
   final String title;
   final String price;
   final String discount;
   final String imageUrl;
   final String? oldPrice;
   final bool showOldPrice;
-  final VoidCallback? onAddTap;
+  final VoidCallback? onTap;
 
   const ProductCard({
     super.key,
+    required this.id,
     required this.title,
     required this.price,
     required this.discount,
     required this.imageUrl,
     this.oldPrice,
     this.showOldPrice = false,
-    this.onAddTap,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 140,
-      margin: const EdgeInsets.only(right: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              Container(
-                width: 170, 
-                height: 110, 
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFAF6EF),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                clipBehavior: Clip.hardEdge,
-                child: AspectRatio(
-                  aspectRatio: 4 / 3, 
+    final quantity = context.watch<QuantityProvider>().getQuantity(id);
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 140,
+        margin: const EdgeInsets.only(right: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: 170,
+                  height: 110,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFAF6EF),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  clipBehavior: Clip.hardEdge,
                   child: CachedNetworkImage(
                     imageUrl: imageUrl,
                     fit: BoxFit.cover,
@@ -49,86 +55,133 @@ class ProductCard extends StatelessWidget {
                       baseColor: Colors.grey.shade300,
                       highlightColor: Colors.grey.shade100,
                       child: Container(
-                        color: Colors.grey[300],
+                        color: Colors.grey.shade300,
                       ),
                     ),
-                    errorWidget: (context, url, error) => const Icon(
-                      Icons.error,
-                      size: 40,
-                      color: Colors.redAccent,
-                    ),
+                    errorWidget: (context, url, error) => const Icon(Icons.error),
                   ),
                 ),
-              ),
-              Positioned(
-                top: 6,
-                left: 6,
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                  decoration: BoxDecoration(
-                    color: Colors.red.shade400,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Text(
-                    discount,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 6,
-                right: 6,
-                child: GestureDetector(
-                  onTap: onAddTap,
+                Positioned(
+                  top: 6,
+                  left: 6,
                   child: Container(
-                    height: 32,
-                    width: 32,
+                    padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
                     decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.black12),
+                      color: Colors.red.shade400,
+                      borderRadius: BorderRadius.circular(4),
                     ),
-                    child: const Icon(Icons.add, size: 20),
+                    child: Text(
+                      discount,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          Text(
-            title,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              height: 1.3,
+                Positioned(
+                  bottom: 6,
+                  right: 6,
+                  child: quantity > 0
+                      ? Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.black12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12.withOpacity(0.05),
+                                blurRadius: 4,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  if (quantity == 1) {
+                                    context.read<QuantityProvider>().remove(id);
+                                  } else {
+                                    context.read<QuantityProvider>().decrease(id);
+                                  }
+                                },
+                                child: Icon(
+                                  quantity == 1 ? Icons.delete : Icons.remove,
+                                  size: 20,
+                                  color: quantity == 1 ? Colors.red : Colors.black,
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              Text(
+                                quantity.toString(),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              GestureDetector(
+                                onTap: () {
+                                  context.read<QuantityProvider>().increase(id);
+                                },
+                                child: const Icon(Icons.add, size: 20),
+                              ),
+                            ],
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            context.read<QuantityProvider>().increase(id);
+                          },
+                          child: Container(
+                            height: 32,
+                            width: 32,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(color: Colors.black12),
+                            ),
+                            child: const Icon(Icons.add, size: 20),
+                          ),
+                        ),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            price,
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF179C52),
-            ),
-          ),
-          if (showOldPrice && oldPrice != null)
+            const SizedBox(height: 8),
             Text(
-              oldPrice!,
+              title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
               style: const TextStyle(
-                fontSize: 12,
-                color: Colors.black38,
-                decoration: TextDecoration.lineThrough,
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                height: 1.3,
               ),
             ),
-        ],
+            const SizedBox(height: 4),
+            Text(
+              price,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF179C52),
+              ),
+            ),
+            if (showOldPrice && oldPrice != null)
+              Text(
+                oldPrice!,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.black38,
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
